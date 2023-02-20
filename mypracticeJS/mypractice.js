f01ec1aabd42ec57ab172a0b578812a75b64e450
dlfdydwlrshehdwk <118067728+dlfdydwlrshehdwk@users.noreply.github.com>
2023-02-20 17:11:09 +0900

    1

---
+++ mypracticeJS/mypractice.js
@@ -0,0 +1,7 @@
+window.addEventListener("DOMContentLoaded",()=>{
+
+    const btn = document.querySelector('.btn');
+    const screen = document.querySelector('.screen');
+    btn.onclick = ()=>{screen.classList.toggle('on')};
+
+});
