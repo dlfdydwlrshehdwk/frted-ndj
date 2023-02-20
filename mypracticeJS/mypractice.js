40a34d4c6105c980ae944e2bd6d31fdb35f65eb7
dlfdydwlrshehdwk <fkrmsk147@naver.com>
2023-02-20 23:51:29 +0900

    1

--- mypracticeJS/mypractice.js
+++ mypracticeJS/mypractice.js
@@ -1,14 +1,39 @@
 
-window.addEventListener('DOMContentLoaded', 함수구역);
+window.addEventListener("DOMContentLoaded",()=>{
+    console.log("로딩완료");
+    const btns = document.querySelectorAll(".btns button");
+    const slide = document.querySelector('.slidect');
+    const next = document.querySelector('.next');
 
+    btns.forEach((ele,idx)=>{
+        ele.onclick=()=>{
+            console.log(ele,idx);
+        
+        slide.style.transform = `translateX(${'-' + idx * '100' + 'vw'})`;
+        console.log(`${'-' + idx * '100'}`)
+        
+        
+        // if(idx===1){
+        //     slide.style.transform = 'translateX(-100vw)';
+        // }
+        // else if(idx===2){
+        //     slide.style.transform = 'translateX(-200vw)';
+        // }
+        // else{
+        //     slide.style.transform = 'translateX(0)';
+        // }
+        
 
-function 함수구역(){
-    const myvid = document.querySelector("#myvid");
-    myvid.addEventListener("timeupdate",check);
+        }
 
-    function check(){
-        if(myvid.paused){
-            location.href="index.html";
+    })
+    let i = 1;
+    next.onclick=()=>{
+        slide.style.transform=`translateX(${'-' + i *'100' + 'vw'})`;
+        i++;
+        if(i==3){
+            i = 0;
         }
     }
-}
+
+});
