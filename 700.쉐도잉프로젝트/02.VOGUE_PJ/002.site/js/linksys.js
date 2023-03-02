de69b621cae7221b0001cc84c8415685fc180b18
dlfdydwlrshehdwk <118067728+dlfdydwlrshehdwk@users.noreply.github.com>
2023-03-02 15:43:14 +0900

    1

--- 700.쉐도잉프로젝트/02.VOGUE_PJ/002.site/js/linksys.js
+++ 700.쉐도잉프로젝트/02.VOGUE_PJ/002.site/js/linksys.js
@@ -7,7 +7,41 @@ window.addEventListener('DOMContentLoaded', linkFn);
 function linkFn(){
     console.log('링크 로딩완료');
     
+    // 1. 링크 대상 선정 : 
+    // (1) GNB : .gnb a
+    const gnb = document.querySelectorAll('.gnb a');
+    // (2) 로고 : .logo a
+    const logo = document.querySelector('.logo a');
+    // console.log('gnb:',gnb,'로고:',logo);
 
+    // 2. 클릭이벤트 설정하기
+    // (1) GNB 클릭설정
+    for(let x of gnb){
+        x.onclick = (e) => {
+            // 클릭이동기능막기
+            e.preventDefault();
+            // (1) 클릭된 a요소 텍스트 읽기 + 소문자로변환 + 앞뒤 공백제거
+            let atxt = x.innerText.toLowerCase().trim();
+            // toLowerCase() -> 소문자변환
+            // toUpperCase() -> 대문자변환
+            // trim() -> 앞뒤 공백제거
+            
+            console.log(atxt);
+
+            // (2) 서브 페이지 이동하기
+            if(atxt !== 'search'){ // 검색이 아니라면 이동해!
+                location.href = "category.html?cat="+encodeURIComponent(atxt);
+            }
+            
+        };//click ////
+    }// for of ////
+
+    // (2) 로고 클릭설정
+    logo.onclick = (e) => {
+        e.preventDefault();
+        // 홈으로 이동하기
+        location.href = "index.html";
+    }; // click ////
 
 
 
