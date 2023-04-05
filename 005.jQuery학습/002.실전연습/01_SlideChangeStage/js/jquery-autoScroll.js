b3498ba597e102b68d3fbd06e29a7899629d1063
dlfdydwlrshehdwk <118067728+dlfdydwlrshehdwk@users.noreply.github.com>
2023-04-05 16:23:09 +0900

    7

--- 005.jQuery학습/002.실전연습/01_SlideChangeStage/js/jquery-autoScroll.js
+++ 005.jQuery학습/002.실전연습/01_SlideChangeStage/js/jquery-autoScroll.js
@@ -1,323 +1,151 @@
-// JS로 구현한 자동페이지 휠 JS - js-autoScroll.js
+// JS로 구현한 자동페이지 휠 JS - jquery-autoScroll
 
-// 새로고침 할때 스크롤위치 캐싱 무시하고 맨위로 이동
-// scrollTo(가로,세로) -> 위치이동 메서드
-setTimeout(()=>{
-    window.scrollTo(0,0);
-},100)
+// 로딩구역없이 함수로 구현함 ////
 
-// 로딩함수 호출 ////
-window.addEventListener('DOMContentLoaded', loadFn);
+/********************************************** 
+    대상 변수할당하기
+**********************************************/
+// 전체 페이지번호
+let pno = 0;
+// 페이지요소
+const pg = $('.page')
+// 전체 페이지개수
+const pgcnt = pg.length;
+console.log('페이지개수',pgcnt,pg);
+// 광실행 금지변수
+let prot = [];
+// 광스크롤금지 
+prot[0] = 0;
+// gnb메뉴 li
+const gnb = $('.gnb li');
+// 인디케이터
+const indic = $('.indic li');
+// 각 페이지별 등장 
+const minfo = $('.minfo');
 
-/***************************************** 
-    함수명 : loadFn
-    기능 : 페이지 로딩시 기능수행
-*****************************************/
+/********************************************** 
+ 이벤트 등록하기
+ **********************************************/
+ // 윈도우 휠이벤트 발생시
+ $(window).on('wheel',wheelFn);
+// gnb메뉴 클릭시 : 대상 - .gnb a
+$('.gnb a').on('click',chgMenu);
+// 인디케이터 클릭시 : 대상 - .indic a
+$('.indic a').click(chgMenu);
 
-function loadFn() {
+// 새로고침시 스크롤위치 캐싱 변경하기(맨위로!)
+$('html,body').animate({scrollTop : '0px'})
 
-    console.log('로딩완료');
+/********************************************* 
+    함수명 : wheelFn
+    기능 : 마우스휠 이벤트 발생시 호출됨
+    -> 한페이지씩 자동스크롤 기능
+*********************************************/
 
-    // [ 이벤트 연결 대상선정하기 ] ////
-    // (1) GNB메뉴 
-    const gnb = document.querySelectorAll('.gnb a');
-    console.log(gnb);
-    // (2) 인디케이터메뉴 
-    const indic = document.querySelectorAll('.indic a');
-    console.log(indic);
+function wheelFn(){
+    // 광휠금지
+    if(prot[0] === 1) return;
+    chkCrazy(0);
 
-    // [ 이벤트 연결 함수등록하기 ] ////
-    // (1) GNB메뉴 이벤트연결
-    gnb.forEach((ele,idx)=>{//요소 , 순번
-        ele.addEventListener('click',()=>movePg(idx));
-        // 전체 객체(obj)를 함수에 전달하는 이유는?
-        // -> 인디케이터도 GNB와 같은 기능을 수행하기 때문에
-        // 호출시 자기자신ㅇ 전체를 보내야 각각에 맞게 기능을 수행할 수 있다.
+    console.log('?');
 
-    }); // forEach ////
+    // 1. 휠방향  알아내기
+    let delta = event.wheelDelta
+    
+    // 2. 방향에 따른 페이지번호 증감
+    if(delta < 0){
+        pno++;
+        if(pno===pgcnt){
+            pno = pgcnt - 1;
+        } // 마지막 페이지번호에고정
+    } //if ////
+    else{
+        pno--;
+        if(pno === -1) pno = 0;
+        // 첫페이지번호에고정
+    } // else  
 
-    // (2) 인디케이터메뉴 이벤트연결
-    indic.forEach((ele,idx)=>{//요소 , 순번
-        ele.addEventListener('click',()=>movePg(idx));
-    }); // forEach ////
+    // 3. 스크롤 이동하기 + 메뉴에 클래스 'on'넣기
+    movePg();
 
-
-
-
-
-    /********************************************* 
-        [ 휠 이벤트를 이용한 페이지 이동 컨트롤 하기 ]
-
-        -> 휠 이벤트명 : wheel
-        (예전에는 mousewheel 이벤트가 사용됨 - 공식적으로 폐기됨)
-
-        1. 사용법 : 
-            (1) 이벤트속성에 함수를 할당
-                요소.onwheel = 함수
-            (2) 브라우저 이벤트에 등록함
-                요소.addEventListener('wheel', 함수, {옵션})
-            
-        2. 포인트 :
-            (1) 기존 휠이동 기능은 정지한다.
-            -> event.preventDefault()
-            (2) 휠 방향을 알아내어 기능과 매칭한다.
-            -> event.wheelDelta (휠 방향속성)
-            (3) 페이지번호를 전역적으로 사용한다.
-            -> let pgnum = 0
-
-        3. 휠 이벤트 대상 : 
-            (1) 전체 스크롤바인 경우 : window
-            (2) 개별박스인 경우 : 선택요소
-
-        4. scroll 이벤트와 wheel 이벤트 비교
-            (1) 공통점 : 스크롤되는 페이지의 위치이동
-            (2) 차이점 : 마우스 휠의 동작에만 반응하는 이벤트는 wheel
-                스크롤바의 이동에 반응하는 이벤트는 scroll
-        ___________________________________________________________
-
-        [ addEventListener 메서드의 옵션에 관하여 ]
-
-        기존 addEventListener의 3번째 파라미터로 캡쳐링/버블링 여부를 
-        제어할 수 있는 부분이 EventListenerOptions이라는 객체형태의 
-        추가 옵션을 받을수 있음
-
-        EventListenerOptions 사용 전
-        document.addEventListener('touchstart', handler, false);
-
-        EventListenerOptions 사용 후
-        document.addEventListener("touchstart", handler, {
-            capture: false,
-            once: false,
-            passive: false
-        });
-
-        ※ 현재 크롬에서 지원하는 EventListenerOptions 옵션은 다음과 같다.
-
-        (1) capture: 이벤트 캡쳐링 적용 여부. 크롬 49부터 지원
-        (2) once: 이벤트를 한번만 호출하고 해제되는 옵션. 크롬 55부터 지원
-        (3) passive: 스크롤 성능 향상을 위한 옵션으로 true일 경우, 
-                스크롤을 위해 블록되는 것을 방지함. 이 경우, 
-                preventDefault를 사용할 수 없음. 크롬 51부터 지원
-                이 중, passive 속성은 성능향상을 위해, 
-                브라우저의 기능을 프로그래밍으로 제어할수 있음
-                -> 최근 업데이트된 브라우저는 passiv 기본값이 true로 셋팅됨으로
-                window, document, body 이 세가지 객체에 대해 스크롤 막기 기능을
-                비활성화 하였다.
-                따라서 기본기능을 막고자 하면 passive:false
-                로 기능을 활성화 해야한다.
-
-    *********************************************/
-
-        // 0. 변수 설정하기
-        // (1) 전체 페이지 변수 
-        let pgnum = 0; // 현재 페이지 번호(첫페이지 0)
-        // (2) 전체 페이지 수 
-        const pgcnt = document.querySelectorAll('.page').length;
-        console.log(pgcnt);
-        // (3) 광스크롤 금지변수 ( 0 - 허용, 1 - 불허용)
-        let prot_sc = 0;
-        
+} // wheelFn 함수  ////
 
 
-        // 1. 전체 휠 이벤트 설정하기
-        window.addEventListener('wheel',wheelFn,{passive:false});
-
-        // 2. 휠 이벤트 함수 만들기
-        function wheelFn(e) {
-            // (0) 기본기능 멈추기
-            // addEventListener 옵션 passive:false 필수!
-            e.preventDefault();
-
-            // 광스크롤 막기 ////
-            if(prot_sc === 1) return;
-            prot_sc = 1; // 신호 1개만 허용
-            setTimeout(()=>{
-                prot_sc = 0;
-            },800); 
-            // 0.8초의 시간 후 다시 허용상태전환
-
-            // (1) 호출확인 
-            // console.log('휘이잉ㄹ');
-
-            // (2) 휠 방향 알아내기
-            // 이벤트객체.wheelDelta
-            let dir = e.wheelDelta;
-            console.log('방향:',dir);
-            // 휠 내리면 마이너스(올리면 플러스)
-            
-            // (3) 방향에 따른 페이지번호 증감
-            // 페이지 밑방향 : 페이지번호 증가
-            if(dir<0) {
-                // 페이지번호 1씩증가
-                pgnum++;
-                // 한계수 : 페이지 끝번호(페이지수 -1)
-                if(pgnum > pgcnt - 1) pgnum = pgcnt - 1;
-            }// if ////
-            
-            // 스크롤 윗방향 : 페이지 번호 감소
-            else {
-                // 페이지번호 1씩 감소
-                pgnum--;
-                // 한계수 : 페이지 첫번호 0
-                if(pgnum < 0) pgnum = 0;
-            } // else ////
-            console.log('페이지번호:',pgnum);
-
-            // (4) 페이지 이동하기 + 메뉴변경 - updatePg함수 호출
-            updatePg(gnb);
-            updatePg(indic);
-        }// wheelFn 함수 ////
-
-        /********************************* 
-            함수명 : movePg
-            기능 : GNB메뉴 클릭시 해당 페이지로 이동하기
-        *********************************/
-        function movePg(seq){ // seq - 순번
-            // 1.기본기능막기
-            event.preventDefault();
-            // 2.호출확인
-            console.log('무브피지',seq)
-            // 3. 페이지번호(pgnum) 업데이트하기
-            pgnum = seq;
-            // console.log('클릭',pgnum);
-            // 4. 업데이트 페이지 호출 -> 페이지이동, 메뉴변경
-            // 개별객체를 업데이트 할때는 obj가 필요했으나
-            // GNB메뉴와 인디케이터가 모두 업데이트 돼야하므로
-            // 개별 obj가 필요없게됨
-            updatePg(gnb);
-            updatePg(indic);
-
-        }// movePg함수 ////
-        
-        /******************************************** 
-            함수명 : updatePg
-            기능 : 페이지 이동시 설정값 업데이트 하기
-        ********************************************/
-        function updatePg(obj) { // obj - 변경할 메뉴전체 객체
-            // 1. 함수호출
-            // console.log('업데이트')
-            // 2. 페이지 이동하기
-            // scrollTo(가로,세로)
-            window.scrollTo(0,window.innerHeight*pgnum);
-            // 세로 이동위치 : 윈도우 높이값* 페이지번호
-            // 3. 메뉴초기화(클래스 on 제거하기)
-            for(let x of obj){ //x는 gnb a요소 패렌츠는 li요소
-                console.log(obj)
-                 x.parentElement.classList.remove('on');
-                //  console.log(x)
-                }
-            // 4. 해당메뉴에 클래스 on 넣기
-            obj[pgnum].parentElement.classList.add('on');
-
-            // 5. 페이지 이동후 해당 페이지액션
-            // pageAction 함수호출 (페이지이동 시차를 1초정도줌)
-            setTimeout(()=>pageAction(pgnum),1000)
-        } // updatePg 함수 ////
+// 광클초기값
+prot[1] = 0;
+/*********************************************** 
+    함수명 : chgMenu 
+    기능 : 메뉴 클릭시 메뉴변경과 페이지이동
+***********************************************/
+function chgMenu() {
+    // 0. 광클금지
+    if(prot[1]) return;
+    chkCrazy(1);
 
 
-        /**************************************** 
-            함수명 : initCSS
-            기능 : 등장할 요소들의 초기값 셋팅
-        ****************************************/
-        // 1. 대상 : .minfo
-        const minfo = document.querySelectorAll('.minfo');
-        console.log(minfo);
-        // 2. 이벤트 설정
-        minfo.forEach((ele,idx)=>{initCSS(ele,idx)});
-        // 3. 함수만들기
-        function initCSS(ele,seq) { //ele는 요소, seq는 순번
-            // 1. 호출확인
-            // console.log('이닛호출',seq);
+    // 1. 클릭된 a요소의 부모 li 순번을 구함 === pno
+    let idx = $(this).parent().index();
+    console.log('나클',this,idx);
 
-            // 2. 해당요소 스타일속성 선택
-            let sty = ele.style; 
+    // 2. 전역페이지번호에 순번 업데이트
+    pno = idx;
 
-            // 3. 각 요소별 초기화하기
+    // 3. 페이지이동 + 메뉴에 클래스 'on'넣기
+    movePg();
+    
+} // chgMenu 함수 ////
 
-            // 트랜지션 공통초기화 
-            sty.transition = 'none';
-
-            // 페이지별 초기화
-            if(seq === 0){ // 1번페이지
-                sty.left = '130%'
-            }
-            else if(seq === 1){ // 2번페이지
-                // 투명하게
-                sty.opacity = 0;
-            }
-            else if(seq === 2){ // 3번페이지
-                sty.top ='-20%'
-            }
-            else if(seq === 3){ // 4번페이지
-                
-                sty.transform = 'translate(-50%, -50%) scale(0)'
-            }
-            else if(seq === 4){ // 5번페이지
-                sty.top  = '-30%'
-                sty.left = '130%'
-                sty.transform = 'translate(-50%, -50%) rotate(1800deg)'
-            }
-            else if(seq === 5){ // 6번페이지
-                sty.top ='-20%'
-            }
-            else if(seq === 6){ // 7번페이지
-                sty.top ='-20%'
-            }
-        }// initCSS 함수 ////
-
-        /************************************* 
-            함수명 : pageAction
-            기능 : 페이지별 액션주기
-        *************************************/
-        function pageAction(seq) { // seq - 변경순번
-            // 1. 호출확인
-            console.log('액션',seq);
-
-            // 2. 변경대상 스타일 속성선택
-            let sty = minfo[seq].style;
-            // 3. 전체 초기화!
-            minfo.forEach((ele,idx)=>{initCSS(ele,idx)});
+/*********************************************** 
+    함수명 : chkCrazy 
+    기능 : 광적동작 체크하여 제어리턴
+***********************************************/
 
 
-            // 4. 해당페이지 액션주기
-            if(seq === 0){
-                sty.left = '50%';
-                sty.transition = '1s  cubic-bezier(0.63, -0.91, 0.62, 1.46) 0s';
-            }
-            else if(seq ===1){
-                // 투명도 복원하기
-                sty.opacity = 1;
-                // 트랜지션 주기
-                sty.transition = '1.5s ease-in';
-            }
-            else if(seq ===2){
-                sty.top = '50%';
-                sty.transition = '1s  cubic-bezier(0.63, -0.91, 0.62, 1.46) 0s';
-            }
-            else if(seq ===3){
-                sty.transform = 'translate(-50%, -50%) scale(100%) ';
-                sty.transition = '1s ease-in-out'
-            }
-            else if(seq ===4){
-                sty.top = '50%'
-                sty.left = '50%'
-                sty.transform = 'translate(-50%, -50%) rotate(0deg)';
-                sty.transition = '2s ease-in-out';
-            }
-            else if(seq ===5){
-                sty.top = '50%'
-            }
-            else if(seq ===6){
-                sty.top = '50%'
-                sty.transition = '1s  cubic-bezier(0.63, -0.91, 0.62, 1.46) 0s';
-            }
-        } //pageAction 함수 ////
+function chkCrazy(seq){ // seq 관리변수 순번
+    prot[seq] = 1;
+    setTimeout(()=>prot[seq] = 0 ,800)
+} // chkCrazy 함수 ////
 
-        // 첫페이지 시작시 실행
-        // 페이지업데이트( 초록글씨 )
-        setTimeout(()=>updatePg(gnb),100)
-        // 애니메이션실행
-        setTimeout(()=>pageAction(0),1000)
+/*********************************************** 
+    함수명 : movePg
+    기능 : 페이지이동 애니메이션
+***********************************************/
+function movePg(){
+    // 대상 : html, body -> 두개를 모두 잡아야 공통적으로 적용됨
+    $('html,body').animate({
+        scrollTop : ($(window).height() *pno)+'px'
+    },800,'easeInOutQuint',showEle // 이동후 콜백함수호출
+    );
+
+    // 대상 : gnb메뉴 , 인디케이터 메뉴
+    gnb.eq(pno).addClass('on').siblings().removeClass('on');
+    indic.eq(pno).addClass('on').siblings().removeClass('on');
+} // movePg 함수 ////
 
 
-}// loadFn 함수 ////
+// 등장할 요소 초기화 
+minfo.css({
+    opacity : 0,
+    transform: 'translate(-50%,50%)',
+    transition : '.6s ease-out',
+}); // css ////
+
+/*********************************************** 
+    함수명 : showEle
+    기능 : 페이지이동후 요소 등장하기
+***********************************************/
+function showEle(){
+    // .minfo 페이지별 등장하기
+    pg.eq(pno).find('.minfo').css({
+        opacity : 1,
+        transform: 'translate(-50%,-50%)',
+    })
+    .parent('.page').siblings().find('.minfo').css({
+        opacity : 0,
+        transform: 'translate(-50%,50%)',
+        transition : '.6s ease-out',
+    });
+} // showEle 함수 ////
+
+// 최초호출
+setTimeout(showEle,1000)
