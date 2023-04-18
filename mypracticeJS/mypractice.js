2ff22ca04655de0976f26f5a3a2bfd9118af3c9b
dlfdydwlrshehdwk <118067728+dlfdydwlrshehdwk@users.noreply.github.com>
2023-04-18 17:19:42 +0900

    3

--- mypracticeJS/mypractice.js
+++ mypracticeJS/mypractice.js
@@ -1,2 +1,16 @@
 $(()=>{
+
+    const wrap = $('.wrap')
+    wrap.hover(function(){
+        console.log('hi')
+        $('.c1,.rt1,.pg1').css({
+            strokeDashoffset: 0,
+        })
+    },function(){
+        console.log('bye')
+        $('.c1,.rt1,.pg1').css({
+            strokeDashoffset: '',
+        })
+    })
+
 })
