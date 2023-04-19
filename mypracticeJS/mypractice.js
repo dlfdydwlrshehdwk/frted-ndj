ff5524bba151a446ab3daf0b098389714c22a3e7
dlfdydwlrshehdwk <118067728+dlfdydwlrshehdwk@users.noreply.github.com>
2023-04-19 17:20:21 +0900

    3

--- mypracticeJS/mypractice.js
+++ mypracticeJS/mypractice.js
@@ -5,12 +5,36 @@ $(()=>{
         console.log('hi')
         $('.c1,.rt1,.pg1').css({
             strokeDashoffset: 0,
+        });
+        $('.svg3').css({
+            top : '0%',
         })
     },function(){
         console.log('bye')
         $('.c1,.rt1,.pg1').css({
-            strokeDashoffset: '',
-        })
+            strokeDashoffset: '-1200',
+        });
     })
 
-})
+
+
+
+    // $('.wrap').hover(
+    //     function(){
+    //         $('.c1').css({
+    //             strokeDashoffset:0,
+    //         })
+    //     },
+    //     function(){
+    //         $('.c1').css({
+    //             transition : '.8s ease-in-out', 
+    //             strokeDashoffset:-1200,
+    //         });
+    //         setTimeout(()=>{
+    //             $('.c1').css({
+    //                 transition : 'none',
+    //                 strokeDashoffset:1200,
+    //             });
+    //         },800)
+    //     })
+}); // ㄹ드구역
