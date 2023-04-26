7497edfff05df06f2829f0de7c0ca311cb637ade
dlfdydwlrshehdwk <118067728+dlfdydwlrshehdwk@users.noreply.github.com>
2023-04-26 16:21:47 +0900

    1

--- 010.VueJS학습/001.기초연습/js/small.js
+++ 010.VueJS학습/001.기초연습/js/small.js
@@ -1,77 +1,36 @@
 // 쇼핑몰갤러리 JS - small.js
 
-$(()=>{ // JQB //
+// 뷰 JS 인스턴스 생성용 함수 !
+const makeVue = (x) => new Vue({ el: x });
 
-    /* 
-        [ 컴포넌트 데이터전달 / 이벤트호출 ]
-        1. 부모 / 자식 구분하기
-            1) 부모 : 뷰 인스턴스 및 html 뷰대상요소
-            -> 뷰대상요소 내부의 사용자정의태그 
-            2) 자식 : 뷰 컴포넌트 인스턴스
-
-        2. 부모의 변수를 자식에게 전달하기 
-            1) props down : 속성으로 전달하고 props로 받기
-            2) 구현방법 : 
-                (1) 부모 사용자태그에 v-bind:내속성=값
-                    설정한 후 ... 
-                (2) 자식컴포넌트 인스턴스 옵션에 
-                    props:[속성변수명,...] 또는
-                    props : {속성명 : 데이터형, 속성명 : 데이터형, ...}
-                    -> 배열로 값을 셋팅하면 데이터형무관
-                    -> 객체로 값을 셋팅하면 데이터형필터
-                -> 주의! : 부모로부터 받은 데이터변수는 값을 
-                새로 할당하여 변경할 수 없다.
-
-        3. 자식 컴포넌트에서 부모의 이벤트 호출하기
-            1) $emit 으로 전달하고 (1~3) 
-                on이벤트설정으로 받음 (4,5)
-            2) 구현방법: 
-                (1) 자식 컴포넌트 템플릿에서 
-                    v-on:이벤트명=자식메서드 형태로 쓴후
-                (2) 자식 컴포넌트 메서드구역(methods)에 
-                    이벤트에 쓴 자식메서드를 만들고
-                (3) this.$emit(부모가만든이벤트명) 으로 전달
-                (4) 부모 태그에서 
-                    v-on:부모가만든이벤트명=부모메서드
-                    형태로 쓴 후  
-                (5) 부모의 뷰인스턴스 메서드구역(methods)에 
-                    같은 이름의 메서드를 만들고 기능을 구현함
-
-    */
-
-    // 뷰 JS 인스턴스 생성용 함수 !
-    const makeVue = x => new Vue({el:x})
-
-    // 1. 제목에 넣은 전역컴포넌트 만들기
-    Vue.component('tit-comp',{
-        template:`
+// 1. 제목에 넣은 전역컴포넌트 만들기
+Vue.component("tit-comp", {
+    template: `
             <strong>
-                <span>Vue JS 컴포넌트 : </span>
-                쇼핑몰 갤러리 리스트
+                <span>다이아나 쇼핑몰</span>
+                Diana Shopping Mall
             </strong> 
-        `
-      }); // 전역 컴포넌트 1 //
+        `,
+}); // 전역 컴포넌트 1 //
 
-    //   뷰 인스턴스 생성하기 : 반드시 컴포넌트 아래에서 작성
-    new Vue({
-        el : '.tit',
+//   뷰 인스턴스 생성하기 : 반드시 컴포넌트 아래에서 작성
+new Vue({
+    el: ".tit",
+});
+// new Vue({
+//     el : '.tit2',
 
-    })
-    // new Vue({
-    //     el : '.tit2',
+// })
+makeVue(".tit2");
 
-    // })
-    makeVue('.tit2')
+// 숫자증감 변수
+let num = 0;
 
-
-    // 숫자증감 변수
-    let num = 0;
-
-    // 2. 갤러리 리스트에 넣을 전역컴포넌트 만들기
-    // 여기가 자식이다.
-    Vue.component("list-comp",{
-        // v-on:click="goPapa" 로 부모이벤트 접근시작
-        template : `
+// 2. 갤러리 리스트에 넣을 전역컴포넌트 만들기
+// 여기가 자식이다.
+Vue.component("list-comp", {
+    // v-on:click="goPapa" 로 부모이벤트 접근시작
+    template: `
         <div>
             <img v-bind:src="gsrc" v-on:click="goPapa"
             v-on:mouseover="ovNow"  alt="dress">
@@ -82,73 +41,143 @@ $(()=>{ // JQB //
         </div>
         `,
 
-        // 부모에서 v-bind:속성명=값 으로 전달한 속성변수를
-        // props:[] / {} 로 받음
-        props:['haha','myseq','endlet'],
-        // props:{'haha':Number}, -> 데이터형 일치
-        // props:{'haha':String}, -> 데이터형 오류메시지나옴
-        // 컴포넌트 내부 변수셋팅
-        data : function(){
-            return{
-                gsrc : `img_gallery/${this.haha}.jpg`,
-                gname : `Sofia`+this.haha+this.endlet+(this.myseq%2?"❗":"❕"),
-                gprice : this.insComma(1234*this.haha) +`원`,
-            }
+    // 부모에서 v-bind:속성명=값 으로 전달한 속성변수를
+    // props:[] / {} 로 받음
+    props: ["haha", "myseq", "endlet"],
+    // props:{'haha':Number}, -> 데이터형 일치
+    // props:{'haha':String}, -> 데이터형 오류메시지나옴
+    // 컴포넌트 내부 변수셋팅
+    data: function () {
+        return {
+            gsrc: `img_gallery/${this.haha}.jpg`,
+            gname: `Sofia` + this.haha + this.endlet + (this.myseq % 2 ? "❗" : "❕"),
+            gprice: this.insComma(1234 * this.haha) + `원`,
+        };
+    },
+    methods: {
+        // 부모이벤트호출 전달하기 : $emit() 메서드사용
+        goPapa() {
+            //  $emit(부모가만든이벤트명)
+            this.$emit("hull");
         },
-        methods: {
-            // 부모이벤트호출 전달하기 : $emit() 메서드사용
-            goPapa(){
-                //  $emit(부모가만든이벤트명) 
-                this.$emit("hull")
-            },
-            ovNow(){
-                this.$emit("gotkimchi")
-                // 부모요소에 v-on:gotkimchi = 메서드명
-                // 이것을 만들어서 사용함
-            },
-
-            // 정규식함수(숫자 세자리마다 콤마해주는기능)
-                    insComma(x) {
-                        return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-                    },
-        }
-    }); // 뷰 JS 컴포넌트 ////
-
-    // 리스트 뷰 인스턴스 생성하기
-    // 여기가 부모입니다.
-    new Vue({
-        el : '.grid',
-        // 부모 뷰인스턴스 메서드구역
-        methods : {
-            // 자식이벤트 전달후 실행메서드
-            goMsg(){
-                alert('자식인 부모에게 이벤트전달 성공');
-            },
-            ovMsg(){
-                console.log('오버!')
-            }
+        ovNow() {
+            this.$emit("gotkimchi");
+            // 부모요소에 v-on:gotkimchi = 메서드명
+            // 이것을 만들어서 사용함
         },
-    });// Vue 인스턴스
+
+        // 정규식함수(숫자 세자리마다 콤마해주는기능)
+        insComma(x) {
+            return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+        },
+    },
+}); // 뷰 JS 컴포넌트 ////
+
+// 리스트 뷰 인스턴스 생성하기
+// 여기가 부모입니다.
+new Vue({
+    el: ".grid",
+    // 부모 뷰인스턴스 메서드구역
+    methods: {
+        // 자식이벤트 전달후 실행메서드
+        goMsg() {
+            // alert("자식인 부모에게 이벤트전달 성공");
+        },
+        ovMsg() {
+            // console.log("오버!");
+        },
+    },
+}); // Vue 인스턴스
+
+// 큰이미지보기 배경박스 컴포넌트 //
+Vue.component("win-comp", {
+    template: `
+    <!-- 큰이미지 배경박스 -->
+    <div id="bgbx">
+        <!-- 오른쪽버튼 -->
+        <a href="#" class="abtn rb">
+            <span class="ir">오른쪽버튼</span>
+        </a>
+        <!-- 왼쪽버튼 -->
+        <a href="#" class="abtn lb">
+            <span class="ir">왼쪽버튼</span>
+        </a>
+        <!-- 닫기버튼 -->
+        <a href="#" class="cbtn">
+            <span class="ir">닫기버튼</span>
+        </a>
+        
+        <!-- 큰이미지 박스 -->
+        <div id="imbx">
+            <!-- 큰 이미지 -->
+            <img src="img_gallery/50.jpg" alt="큰 이미지">
+            <!-- 이미지 설명 -->
+            <h4></h4>
+        </div>
+    </div>
+    `,
+}); // win-comp 컴포넌트 //
+
+// win-comp 뷰JS 인스턴스 생성하기 // 
+new Vue({
+    el:"#pbg",
+    // DOM 이 모두 로딩된 후 실행구역
+    mounted:function(){
+        // [ 제이쿼리 기능구현 ]
+
+        // 공유번호변수
+        let nowNum = 1; 
 
 
-     /* 
-        [ 뷰JS 출력 ]
-        mustache : 
-        {{ expression }}문법을 사용해서 특정 변수를 그대로 렌더링한다.
+        // 1. 갤러리 리스트 클릭시 큰이미지박스 보이기
+        $('.grid > div').click(function(e){
+            e.preventDefault();
 
-        v-text : 
-        특정 태그내에서 사용할 수 있으며 v-text속성을 사용해서 "텍스트"로 렌더링한다. 
-        이 때 렌더링된 텍스트는 해당 태그의 자식 노드로 들어간다.
+            // 1. 클릭된 이미지 경로 읽어오기
+            let isrc = $(this).find('img').attr('src')
+            console.log(isrc)
 
-        v-html : 
-        특정 태그내에서 사용할 수 있으며 v-html속성을 사용해서 "html"로 렌더링한다. 
-        이 때 렌더링된 텍스트는 해당 태그의 자식 노드로 들어간다.
+            // 2. 클릭된 이미지 경로를 큰 이미지 src로 넣기
+            $('#imbx').find('img').attr('src',isrc)
 
-        v-bind : 
-        특정 태그내에서 사용할 수 있으며 v-bind문법을 사용해서 vue내부의 값
-        (data, props, computed, methods)들의 값을 해당 속성의 값으로 렌더링한다.
-        */
+            // 3. 큰이미지 박스 보이기
+            $('#bgbx').show()
 
+            // 4. 다음/이전 이미지 변경을 위한 data-num 속성읽기
+            nowNum = $(this).attr("data-num")
+            console.log(nowNum)
 
-    })
-; // JQB //
+            
+        }) ; // click //
+
+        // 2. 닫기버튼 클릭시 큰이미지 박스 숨기기
+        $('.cbtn').click(function(e){
+            e.preventDefault();
+            $('#bgbx').hide();
+
+        })
+
+        // 3. 이전 / 다음버튼 클릭시 이미지변경하기
+        $(".abtn").click(function(e){
+            e.preventDefault();
+            // 오른쪽버튼여부 
+            let isB = $(this).is('.rb');
+            // 분기하기
+            if(isB){ // 오른쪽
+                nowNum++;
+                if(nowNum === 51) nowNum = 1;
+            }
+            else{ // 왼쪽
+                nowNum--; 
+                if(nowNum === 0) nowNum = 50;
+            }
+
+            console.log(nowNum)
+
+            // 4. 큰이미지 변경하기
+            $('#imbx').find('img').attr('src',`img_gallery/${nowNum}.jpg`);
+        })
+
+    }, // mounted 함수구역 //
+
+}); // Vue 인스턴스 // 
