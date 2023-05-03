ee2bdcdf7ff2360ee43d4f10865da103327c4bdf
dlfdydwlrshehdwk <118067728+dlfdydwlrshehdwk@users.noreply.github.com>
2023-05-03 12:12:41 +0900

    1

--- 700.쉐도잉프로젝트/03.Pilot_PJ/002.구현소스/js/sub.js
+++ 700.쉐도잉프로젝트/03.Pilot_PJ/002.구현소스/js/sub.js
@@ -4,6 +4,8 @@
 import menuFn from "./mainjs/menu.js";
 // 공통데이터 가져오기
 import comData from "./tempData/data-common.js";
+// 신상정보 가져오기
+import sinsang from "./gdsData/sinsang.js";
 
 // 상단영역 메뉴 뷰 템플릿 셋팅 //
 // Vue.component(작명,{옵션})
@@ -40,6 +42,9 @@ new Vue({
 
         // 부드러운스크롤
         startSS();
+
+        // 신상품 기능함수 호출
+        sinsangFn();
     },
 }) // 상단영역 뷰인스턴스
 
@@ -73,3 +78,62 @@ function makeSwiper() {
     });
 
 } // makeSwiper 함수 //
+
+// 신상품 기능구현 함수 //
+function sinsangFn(){
+
+
+
+/**************************************************    
+    함수명 : moveList
+    기능 : 신상품 리스트박스를 연속하여 왼쪽방향으로 흘러가도록 함
+**************************************************/
+// 대상 : .flist 
+const flist = $(".flist");
+// 위치값 변수
+let lpos = 0;
+// 재귀호출 상태값 변수 1이면 호출가능 0이면 호출불가
+let call_sts = 1; 
+
+function moveList(){
+
+    // 1. 이동위치값 (left값) 감소하기
+    lpos--;
+    console.log('위치값',lpos)
+
+    // 2. 한계값 초기화하기 + 첫번째요소 맨뒤로 이동하기
+    if(lpos < -300) {
+        // 위치값 초기화
+        lpos = 0;
+        // 첫번째 li맨뒤로이동
+        flist.append(flist.find("li").first())
+    
+    }
+
+    // 3. 이동하기
+    flist.css({
+        left : lpos + 'px'
+    })
+
+    // 재귀호출하기 ( 비동기 호출 - setTimeout)
+    // 조건 
+    if(call_sts)setTimeout(moveList,40)
+
+
+}  // moveList 함수 //
+
+// 신상품 이동함수 최초호출  
+moveList();
+
+// 신상품 리스트에 마우스 오버시 멈춤
+// 신상품 리스트에 마우스 아웃시 이동
+// hover(함수1,함수2)
+flist.hover(
+    function(){ // 오버
+        call_sts = 0; // 콜백중단
+    },
+    function(){ // 아웃
+        call_sts = 1; // 콜백허용
+        moveList(); // 함수재호출
+    }); // 호버 //
+} // sinsangFn 함수 //
