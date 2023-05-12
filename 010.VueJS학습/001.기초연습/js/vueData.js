27abcdb299bb9af2441d36426905ecd017c5e3a8
dlfdydwlrshehdwk <118067728+dlfdydwlrshehdwk@users.noreply.github.com>
2023-05-12 10:05:50 +0900

    1

---
+++ 010.VueJS학습/001.기초연습/js/vueData.js
@@ -0,0 +1,1 @@
+// 뷰JS 데이터 처리 JS - vueData.js
---
+++ 010.VueJS학습/001.기초연습/js/vuedata-store.js
@@ -0,0 +1,22 @@
+// 뷰JS 데이터처리 뷰엑스스토어 vuedata-store.js
+
+const store = new Vuex.Store({
+    // (1) 데이터 셋팅구역 :
+    state:{
+
+    }, // state 구역 // 
+
+    // (2) 데이터 변경 메서드구역 : 호출시 commit()사용!
+    mutations:{
+
+    },
+    // (3)백엔 관련 코딩 비동기처리 메서드구역 : 호출시 dispatch()사용
+    actions : {
+        
+    },
+
+}); // 뷰엑스 인스턴스 //
+
+
+// 파일내보내기
+export default store;
