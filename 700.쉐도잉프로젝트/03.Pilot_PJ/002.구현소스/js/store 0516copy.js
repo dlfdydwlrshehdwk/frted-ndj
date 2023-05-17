3a90410fe99de19db3e74485f84695b3c6e565fd
dlfdydwlrshehdwk <118067728+dlfdydwlrshehdwk@users.noreply.github.com>
2023-05-17 11:02:41 +0900

    4

--- 700.쉐도잉프로젝트/03.Pilot_PJ/002.구현소스/js/store 0516copy.js
+++ 700.쉐도잉프로젝트/03.Pilot_PJ/002.구현소스/js/store 0516copy.js
@@ -1,67 +1,57 @@
-// 서브 페이지 뷰엑스 스토어 셋팅 JS  - store.js
+// 서브페이지 뷰엑스 스토어 셋팅 JS - store.js
 const store = new Vuex.Store({
+    // 데이터 구역 //
     state: {
         // 서브데이터 셋업
         subData: {
             남성: {
                 // 남성메뉴
                 menu: ["NEW MEN'S ARRIVAL", "WINDBREAKER", "BEACH STYLE", "SPORT STYLE"],
-                // 배너개수
+                // 배너 개수
                 cnt: 3,
-                // 카테고리명
+                // 카테고리 이름
                 cat: "men",
             },
             여성: {
                 // 여성메뉴
                 menu: ["NEW WOMEN'S ARRIVAL", "SPORTY FASHION", "FREE STYLE", "COMFORTABLE STYLE"],
-                // 배너개수
+                // 배너 개수
                 cnt: 3,
-                // 카테고리명
+                // 카테고리 이름
                 cat: "women",
             },
             스타일: {
                 // 스타일메뉴
                 menu: ["SPECIAL SUMMER STYLE", "GOLF LIFE", "CAMPING STYLE", "SPORT STYLE"],
-                // 배너개수
+                // 배너 개수
                 cnt: 5,
-                // 카테고리명
+                // 카테고리 이름
                 cat: "style",
             },
         },
         // 공통처리 메뉴 변수
-        menu: [],
+        menu: ["NEW MEN'S ARRIVAL", "WINDBREAKER", "BEACH STYLE", "SPORT STYLE"],
         // 공통처리 배너개수 변수
-        cnt: 0,
+        cnt: 3,
         // 공통처리 카테고리명 변수
-        cat: "",
-        // 공통처리 분류명
-        name: "",
-        // 상품속성코드(클래스명)
-        cls:"m1",
-        // 상품이름
-        gname:"",
-        // 상품코드
-        gcode:"",
-        // 상품가격
-        gprice:"",
+        cat: "men",
     },
-    // state 데이터 변경 메서드구역!
+    // state 데이터 변경 메서드구역
     mutations: {
         // 데이터 변경 셋업 메서드
-        chgData(dt,pm){ // dt-state데이터, pm-전달값
-            console.log("데이터변경:",pm);
+        chgData(dt,pm){ // dt 는 state데이터 pm은 전달값
+            console.log("데이터변경",pm) 
+
             // pm에 객체데이터 속성명이 전달됨(남성/여성/스타일)
-            // 1. 해당 카테고리 개수 업데이트
+            // 1. 해당 카테고리 개수 업데이트 
             dt.cnt = dt.subData[pm].cnt;
-            // 2. 해당 카테고리 이름 업데이트
+            // 2. 해당 카테고리 이름 업데이트 
             dt.cat = dt.subData[pm].cat;
-            // 3. 해당 카테고리 메뉴 업데이트
+            // 3. 해당 카테고리 메뉴 업데이트 
             dt.menu = dt.subData[pm].menu;
-            // 4. 해당 분류명 업데이트
-            dt.name = pm;
-        }, ///////// chgData메서드 ///////
+
+        }, // chgData 메서드 //
     },
 });
 
-// 내보내기
-export default store;
+export default store; 
---
+++ 700.쉐도잉프로젝트/03.Pilot_PJ/002.구현소스/js/tempData/data-0516copy.js
@@ -0,0 +1,129 @@
+// 서브 data 객체 셋팅 JS - data-sub.js
+
+const subData = {
+    // 배너영역
+    banner : `
+    <section id="ban" class="page">
+        <!-- Swiper -->
+        <div class="swiper mySwiper">
+            <div class="swiper-wrapper">
+                <div class="swiper-slide" 
+                v-for="v in $store.state.cnt"
+                v-bind:key="v">
+                    <img 
+                    v-bind:src="
+                    './images/sub/'+
+                    $store.state.cat+
+                    '/banner/ban'+
+                    v+
+                    '.png'
+                    " alt="서브배너이미지" />
+                </div>
+            </div>
+            <div class="swiper-button-next"></div>
+            <div class="swiper-button-prev"></div>
+            <div class="swiper-pagination"></div>
+        </div>
+    </section>
+    `,
+    // 컨텐츠영역1 : new arrival
+    cont1 : `
+    <section 
+    :class="
+    'cont c1 '+ $store.state.cat
+    " id="c1"
+    :data-cat="$store.state.cat">
+
+    <!-- 
+        class = "cont c1 카테고리명"
+        -> 해당 배경이미지가 나오도록 카테고리명 클래스 넣기
+
+        data-cat 속성 : data-로 시작하는 사용자정의 속성
+        -> 제이쿼리에서 DOM셋팅 속성을 읽어서 
+        sinsang 객체의 하위 속성 카테고리명을 사용하기 위해 셋팅함
+    -->
+
+
+        <!-- 2-1-1.신상품타이틀 -->
+        <h2 class="c1tit js-reveal">{{$store.state.menu[0]}}</h2>
+        <!-- 2-1-2.신상품박스 -->
+        <div class="flowbx js-reveal">
+            <!-- 리스트박스 -->
+            <ul class="flist">
+                <li v-for="v in 9" :class="'m'+v">
+                    <a href="#">
+                        <img :src="
+                        './images/goods/'+
+                        $store.state.cat +
+                        '/m'+ v +'.png'
+                        " alt="신상품" />
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </section>
+    `,
+    // 컨텐츠영역2 : special
+    cont2 : `
+    <section 
+    
+    :class="
+    'cont c2 '+ $store.state.cat"
+     id="c2">
+        <h2 class="c2tit js-reveal">{{$store.state.menu[1]}}</h2>
+    </section>
+    `,
+    // 컨텐츠영역3 : 일반소개1
+    cont3 : `
+    <section class="cont c3" id="c3">
+            <ul class="pgc">
+                <li class="txtc">
+                    <h2 class="js-reveal">
+                        <a href="#"> 
+                        {{$store.state.cat.toUpperCase()}}'S<br />
+                        {{$store.state.menu[2]}} 
+                        </a>
+                    </h2>
+                </li>
+                <li class="imgc">
+                    <img :src="
+                    './images/sub/'
+                    + $store.state.cat + '/03.disc.png'" alt="해변공유" />
+                </li>
+            </ul>
+        </section>
+    `,
+    // 컨텐츠영역4 : 일반소개2
+    cont4 : `
+    <section class="cont c4" id="c4">
+        <ul class="pgc">
+            <li class="imgc">
+                <img :src="
+                './images/sub/'+ $store.state.cat +'/04.disc.png'
+                " alt="공유스타일" />
+            </li>
+            <li class="txtc ">
+                <h2 class="tm">
+                    <a href="#" class="js-reveal"> 
+                    {{$store.state.cat.toUpperCase()}}'s<br />
+                    SPORT STYLE </a>
+                </h2>
+                <h2 class="tw">
+                    <a href="#" class="js-reveal"> 
+                    {{$store.state.cat.toUpperCase()}}'s<br />
+                    LIFE STYLE</a>
+                </h2>
+            </li>
+            <li class="imgc">
+            <img :src="
+            './images/sub/'+ $store.state.cat +'/05.disc.png'
+            " alt="공유스타일" />
+            </li>
+        </ul>
+    </section>
+    `,
+
+
+}; // subData //
+
+export default subData;
