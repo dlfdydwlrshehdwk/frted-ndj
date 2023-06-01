e01fd882558e309d598df9f39aa09b200227e2fb
dlfdydwlrshehdwk <118067728+dlfdydwlrshehdwk@users.noreply.github.com>
2023-06-01 10:20:16 +0900

    1

---
+++ 005.jQuery학습/003.응용구현/jq_board/js/list.js
@@ -0,0 +1,15 @@
+// 리스트페이지 JS - list.js
+
+// [ 제이슨 파일 데이터 로컬스토리지에 넣기 ]
+// 1. 변수에 제이슨 파일 문자화 하여 불러오기 
+let jsn = JSON.stringify(mydata);
+// console.log('제이슨',jsn);
+
+// 2. 로컬스토리지 변수를 설정하여 할당하기
+localStorage.setItem('bdata',jsn);
+// console.log('비데이터',localStorage.getItem('bdata'));
+
+// 3. 로컬스토리지 데이터를 파싱하여 게시판 리스트에 넣기
+// 3-1. 로컬 스토리지 데이터 파싱하기 
+let bdata = JSON.parse(localStorage.getItem('bdata'));
+console.log('로컬파싱',bdata)
