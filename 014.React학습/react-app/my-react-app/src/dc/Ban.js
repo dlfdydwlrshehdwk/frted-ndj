bf7896eb5066dfd67886a0685401130c596b9c6e
dlfdydwlrshehdwk <118067728+dlfdydwlrshehdwk@users.noreply.github.com>
2023-06-01 15:20:54 +0900

    1

--- 014.React학습/react-app/my-react-app/src/dc/Ban.js
+++ 014.React학습/react-app/my-react-app/src/dc/Ban.js
@@ -1,14 +1,40 @@
-function Ban(){
-    return(
-        <>
-            <img src="./images/dcm21.jpg" alt="배너" />
-            <section>
-                <h3>GOTHAM GAZETTE</h3>
-                <h2>WORLDS TRAVELER</h2>
-                <p>Barry who? The Flash isn't the only DC hero this summer<br/> who's been traveling through the multiverse.</p>
-            </section>
-        </>
+// 배너 컴포넌트 - Ban.js
+import "./css/ban.css"
+
+function Ban() {
+
+    const ban_data = [
+        {
+            'src' : './images/dcm21.jpg',
+            "tit" : "GOTHAM GAZETTE",
+            "tit2" : "WORLDS TRAVELER",
+            "cont" : `Barry who? The Flash isn't the only DC hero this summer who's been traveling through the multiverse.`,
+            "btn" : "New Places, Familiar Faces"
+        },
+        {
+            'src' : './images/dcm23.jpg',
+            "tit" : "GET TO KNOW",
+            "tit2" : "CASSANDRA CAIN",
+            "cont" : `Whether it's as Batgirl or Orphan, Cassandra Cain stands as one of the most compelling and beloved players in the Batman mythos.`,
+            "btn" : "MEET CASS"
+        },
+    ];
+
+    return (
+        <div className="banner">
+            <ul>
+                <li>
+                    <img className="banimg" src={ban_data[1]['src']} alt="배너" />
+                    <section className="bantit">
+                        <h3>{ban_data[1]['tit']}</h3>
+                        <h2>{ban_data[1]['tit2']}</h2>
+                        <p>{ban_data[1]['cont']}</p>
+                        <button>{ban_data[1]['btn']}</button>
+                    </section>
+                </li>
+            </ul>
+        </div>
     );
-} // Ban 컴포 // 
+} // Ban 컴포 //
 
 export default Ban;
