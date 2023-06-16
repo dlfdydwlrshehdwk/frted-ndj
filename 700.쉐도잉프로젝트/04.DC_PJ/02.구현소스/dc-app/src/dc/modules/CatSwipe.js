98d9f48ddf98111679759caab13d26c196ee59f3
dlfdydwlrshehdwk <118067728+dlfdydwlrshehdwk@users.noreply.github.com>
2023-06-16 13:20:37 +0900

    1

---
+++ 700.쉐도잉프로젝트/04.DC_PJ/02.구현소스/dc-app/src/dc/modules/CatSwipe.js
@@ -0,0 +1,36 @@
+// CatSwipe 모듈 - CatSwipe.js
+import $ from "jquery";
+import "../css/catswipe.css";
+import SwiperCat from "../plugin/SwiperCat";
+
+// 제이쿼리 로드구역 함수
+function jqFn(){
+    $(()=>{
+
+    }); // JQB // 
+} // jqFn 함수 //
+
+function CatSwipe(props){
+    // props.pg - 페이지별 데이터 구분
+    // props.tit - 모듈타이틀
+    return(
+        <>
+        {/* 코드구역 */}
+        <section className="Catswbox">
+
+        {/* 1. 모듈타이틀 */}
+        <h2 className="tit">{props.tit}</h2>
+        {/* 2. 스와이퍼 컴포넌트 */}
+        <SwiperCat></SwiperCat>
+
+        
+
+        </section>
+        {/* 빈루트를 만들고 JS로드 함수 포함 */}
+        {jqFn()}
+        </>
+        
+    )
+};
+
+export default CatSwipe;
