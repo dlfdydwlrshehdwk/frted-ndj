28908e214e4e32d1c22310406826f64ab553d6dc
dlfdydwlrshehdwk <118067728+dlfdydwlrshehdwk@users.noreply.github.com>
2023-06-20 13:14:46 +0900

    1

--- 700.쉐도잉프로젝트/04.DC_PJ/02.구현소스/dc-app/src/dc/Detail.js
+++ 700.쉐도잉프로젝트/04.DC_PJ/02.구현소스/dc-app/src/dc/Detail.js
@@ -1,36 +1,60 @@
 // 상세페이지 컴포넌트 - Detail.js
 
 import { useLocation } from "react-router-dom";
+import "../../src/dc/css/detail.css";
+import Ban from './modules/Ban'
 
 // 라우터 파라미터값 받아서 데이터 처리!
 
-function Detail(props){
-    // 라우터 전달값을 받기위한 useLocation 생성하기 
+function Detail(props) {
+    // 라우터 전달값을 받기위한 useLocation 생성하기!
     const loc = useLocation();
-    // 보낸 속성명을 변수에 할당하기 
-    // state.속성명 : 내가 라우터를 통해 보낸 속성명 받기
+    // 보내 속성명을 변수에 할당하기
+    // state.속성명 : 내가 라우터를 통해 보낸 속성값 받기
     // 1. 캐릭터이름
     const cname = loc.state.cname;
-    // 2. 캐릭터설명 - '^' 문자로 잘라 배열로 데이터 변경
+    // 2. 캐릭터설명 - '^'문자로 잘라 배열로 데이터로 변경!
     let cdesc = loc.state.cdesc;
-    cdesc = cdesc.split('^');
+    cdesc = cdesc.split("^");
     // 3. 캐릭터명세
-    const facts = loc.state.facts;
+    let facts = loc.state.facts;
+    facts = facts.split("^");
 
-    return(
+    return (
         <>
-            <h2>{cname}</h2>
-            <div className="cdesc">
-                {
-                    cdesc.map(v=><p>{v}</p>)
-                }
-            </div>
-            <div className="facts">
-                <h3>CHARACTER FACTS</h3>
-                {facts}
+        {/* 1. 배너 컴포넌트 넣기 */}
+        <Ban cat={cname}/>
+        {/* 2. 상세정보 박스 */}
+            <div className="detail">
+                <div className="descbx">
+                    <h2>{cname}</h2>
+                    <div className="cdesc">
+                        {cdesc.map((v,i) => (
+                            <p key={i}>{v}</p>
+                        ))}
+                    </div>
                 </div>
+                <div className="facts" style={{textAlign:'left'}}>
+                    <div>
+                        <h3>CHARACTER FACTS</h3>
+                        <table>
+                            <tbody>
+
+                        {
+                            facts.map((v,i)=>
+                            <tr key={i} >
+                                <td>{v.split(":")[0]} : </td>
+                                <td>{v.split(":")[1]}</td>
+                            </tr>
+                            )
+                        }
+                        </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
         </>
-    )
-}; // Detail //
+    );
+} //////////// Detail ////////////////
 
 export default Detail;
--- 700.쉐도잉프로젝트/04.DC_PJ/02.구현소스/dc-app/src/dc/modules/Ban.js
+++ 700.쉐도잉프로젝트/04.DC_PJ/02.구현소스/dc-app/src/dc/modules/Ban.js
@@ -1,140 +1,157 @@
 // 배너 컴포넌트 - Ban.js
+// 배너CSS
 import "../css/ban.css";
-// 배너데이터
+// 배너 데이터
 import ban_data from "../data/banner";
 // 제이쿼리
 import $ from "jquery";
 
 /* 
-    [ JS 혹은 jQuey 라우터 구현시 로드 불일치문제 ]
+    [ JS 혹은 jQuery 라우터구현시 로드 불일치문제 ]
     JS기능이 들어간 페이지의 로드구역 설정시
     본 페이지가 index페이지에 바로 불려질 경우
-    이 문제는 생기지 않는다
-    그러나 라우터로 서브 로딩 구현할 경우 
-    이 구역은 라우터 모체가 로딩되는 것으로 실행된다.
-    따라서 본 모듈에 적용하려고 한 의도와 달리 
-    본 소스에는 적용되지 못한다. -> 라우터 로딩 불일치문제
+    이 문제는 발생하지 않는다!
+    그러나 라우터로 서브 로딩 구현할 경우
+    이 구역은 라우터 모체가로 로딩되는 것으로 실행된다
+    따라서 본 모듈에 적용하려고 한 의도와 달리
+    본 소스에는 적용되지 못한다! -> 라우터로딩 불일치문제!
 
-    [ 해결방법 : 로딩구역을 함수로 만들고 컴포넌트 소스 하단에서 호출함 ]
-    ex) function jqFn(){로드구역을 포함한 소스}
+    [ 해결방법 : 로딩구역을 함수로 만들고 컴포넌트 소스 하단에서 호출함! ]
+    예) function jqFn(){로드구역을 포함한 소스}
         function 컴포넌트(){
             return(
-                <> 소스 
+                <>
+                    소스들
                     {jqFn()}
                 </>
-            );
+            )
         }
 */
+// 로드구역 함수화하기!!! ///////////
+// function jqFn() {} 
+///////////// jqFn 함수 /////////////////
 
-
-// 로드구역 함수화 하기 ! // 
-function jqFn(){
-
-$(()=>{
-
-    // 광클금지 변수 
-    let prot = 0;
-    
-    // 1. 버튼 클릭시 이동기능 구현
-    $('.abtn').click(function(){
-        // 0. 광클금지
-        if(prot) return;
-        prot = 1;
-        setTimeout(()=>{prot = 0;},400)
-
-        // 1. 버튼구분하기
-        let isB = $(this).is(".rb");
-
-        // 슬라이드 타겟설정 : 클릭된 버튼의 형제요소 슬라이더
-        let tg = $(this).siblings('.slider')
-
-        // 2. 분기하여 기능구현하기
-        // (1) 오른쪽버튼 클릭시 : 오른쪽에서 들어옴(left : -100%)
-        if(isB){
-            tg.animate({left : "-100%"},400,
-            function(){ // this sms 타겟
-                // 첫번째 li 맨뒤로 보내기! 동시에 left값 0
-                $(this).append($(this).find('li').first())
-                .css({left:"0"});
-            }); // ani //
-        } // if //
-
-        // (2) 왼쪽버튼 클릭시 : 왼쪽에서 들어옴(left : -100% -> 0)
-        else{
-            // 마지막 li맨앞이동 + 동시에 left : -100% 후 left:0 애니
-            tg.prepend(tg.find('li').last())
-            .css({left:'-100%'})
-            .animate({left:'0'},400);
-        }
-
-        // 3. 배너와 일치하는 블릿에 클래스"on" 넣기
-        // 대상: .indic li
-        // eq(순번) -> 오른쪽이동시 1, 왼쪽이동시0
-        // isB값으로 오른쪽은 true ->1, 왼쪽은 false->0
-        // 순서가 바뀌는 슬라이드에 고유 순번속성 data-seq 값을 읽어옴
-        $('.indic li').eq(tg.find('li').eq(isB).attr('data-seq'))
-        .addClass('on').siblings().removeClass('on'); 
-    });// click //
-
-}); // JQB // 
-
-} // jqFn 함수 //
-
-
-// 반복리스트 코드 생성용 컴포넌트 //
-function MakeList(props) { 
+// 반복리스트 코드 생성용 컴포넌트 ///////
+function MakeList(props) {
     // rec - 개별레코드값(객체형식)
-    // key - 유일키
     return (
         <li data-seq={props.idx}>
             <img className="banimg" src={props.rec["src"]} alt="배너" />
             <section className="bantit">
-                <h3>{props.rec["tit"]}</h3>
-                <h2>{props.rec["tit2"]}</h2>
-                <p>{props.rec["cont"]}</p>
-                <button>{props.rec["btn"]}</button>
+                {
+                    props.rec["tit1"] !== "" &&
+                    <h3>{props.rec["tit1"]}</h3>
+                }
+                {
+                    props.rec["tit2"] !== "" &&
+                    <h2>{props.rec["tit2"]}</h2>
+                }
+                {
+                    props.rec["cont"] !== "" &&
+                    <p>{props.rec["cont"]}</p>
+                }
+                {
+                    props.rec["btn"] !== "" &&
+                    <button>{props.rec["btn"].toUpperCase()}</button>
+                }
+                
+
+                
+                
+               
             </section>
         </li>
     );
-} // MakeList 컴포 //
+} //////////// MakeList //////////////////
 
-// 배너출력용 컴포넌트 //
+// 배너출력용 컴포넌트 /////////
 function Ban(props) {
-    // props.cat 은 배너데이터 구분 속성
+    // props.cat 은 배너데이터 구분속성명
     const sel_data = ban_data[props.cat];
     // sel_data에 담긴값은 data/banner.js의 객체의 배열값
+
+    // 광클금지변수
+    let prot = 0;
+
+    // 이동슬라이드 함수 ////////
+    const goSlide = (e) => {
+        // 1. 이벤트가 발생한 버튼 요소
+        let ele = e.target;
+        // console.log(ele);
+
+        // 0.광클금지
+        if (prot) return;
+        prot = 1;
+        setTimeout(() => (prot = 0), 400);
+
+        // 1. 버튼구분하기
+        let isB = $(ele).is(".rb");
+        // console.log("오른쪽?", isB);
+
+        // 슬라이드 타겟설정 : 클릭된 버튼의 형제요소 슬라이더
+        const tg = $(ele).siblings(".slider");
+
+        // 2. 분기하여 기능구현하기
+        // (1) 오른쪽버튼 클릭시 : 오른쪽에서 들어옴(left:0->-100%)
+        if (isB) {
+            tg.animate({ left: "-100%" }, 400, function () {
+                // this는 타겟!
+                // 첫번째 li 맨뒤로 보내기! 동시에 left:0
+                $(this).append($(this).find("li").first()).css({ left: "0" });
+            }); /////// animate ///////
+        } ///////// if ////////////////
+
+        // (2) 왼쪽버튼 클릭시 : 왼쪽에서 들어옴(left:-100%->0)
+        else {
+            // 마지막 li 맨앞이동+동시에 left:-100% 후 left:0 애니
+            tg.prepend(tg.find("li").last()).css({ left: "-100%" }).animate({ left: "0" }, 400);
+        } //////////// else ////////////////
+
+        // 3. 배너와 일치하는 블릿에 클래스 "on"넣기(나머지는 "on"제거)
+        // 대상: .indic li
+        // eq(순번) -> 오른쪽이동시 1, 왼쪽이동시 0
+        // isB값으로 오른쪽은 true->1, 왼쪽은 false->0
+        // 순서가 바뀌는 슬라이드에 고유 순번속성 data-seq값을 읽어옴!
+        $(ele).siblings(".indic").find("li")
+            .eq(tg.find("li").eq(isB).attr("data-seq"))
+            .addClass("on")
+            .siblings()
+            .removeClass("on");
+    }; //////////// goSlide 함수 /////////////////
+
     return (
         <div className="banner">
-            {/* 이동슬라이드 */}
+            {/* 이동 슬라이드 */}
             <ul className="slider">
-                {
-                    sel_data.map((x,i)=>
-                    <MakeList rec={x} key={i} idx={i} />)
-                }
+                {sel_data.map((x, i) => (
+                    <MakeList rec={x} key={i} idx={i} />
+                ))}
             </ul>
-            {/* 이동 버튼 + 슬라이드 불릿 : 슬라이드가 두개 이상일때*/}
+            {/* 이동버튼 + 슬라이드 블릿 : 슬라이드가 2개이상 */}
             {
-                // 조건식 && 코드 : 조건식이 true 일때 코드 출력
-                sel_data.length > 1 && 
-                <>
-                {/* 양쪽이동버튼 */}
-                <button className='abtn lb'>＜</button>
-                <button className='abtn rb'>＞</button>
-                {/* 블릿 인디케이터 */}
-                <ol className='indic'>
-                    {
-                        sel_data.map((x,i)=>
-                        <li key={i} className={i==0?'on':''}></li>
-                        )
-                    }
-                </ol>
-                </>
+                // 조건식 && 코드 : 조건식이 true일때 코드출력
+                sel_data.length > 1 && (
+                    <>
+                        {/* 양쪽이동버튼 */}
+                        <button className="abtn lb" onClick={goSlide}>
+                            ＜
+                        </button>
+                        <button className="abtn rb" onClick={goSlide}>
+                            ＞
+                        </button>
+                        {/* 블릿 인디케이터 */}
+                        <ol className="indic">
+                            {sel_data.map((x, i) => (
+                                <li className={i == 0 ? "on" : ""} key={i}></li>
+                            ))}
+                        </ol>
+                    </>
+                )
             }
-            {/* JS/jQuery 로드구역 호출 
-            라우터 로드 불일치를 해결함 */}
-            {jqFn()}
+            {/* JS/jQuery 로드구역호출! */}
+            {/* {jqFn()} */}
         </div>
     );
-} // Ban 컴포 //
+} /////////// Ban 컴포넌트 /////////////
 
 export default Ban;
