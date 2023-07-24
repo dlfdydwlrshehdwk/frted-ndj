c4e34e2bf084028cbd4632797fdaaa2179c4ee17
dlfdydwlrshehdwk <fkrmsk147@naver.com>
2023-07-25 07:37:14 +0900

    Update my.js

--- mypracticeJS/my.js
+++ mypracticeJS/my.js
@@ -8,11 +8,19 @@ $(()=>{
             textAlign : 'center',
         })
 
+        // 년
         let year = new Date().getFullYear();
-        let hour = new Date().getHours();
-        let minute = new Date().getMinutes();
-        let day = new Date().getDay();
+        // 월
+        let month = new Date().getMonth() + 1;
+        // 일
         let date = new Date().getDate();
+        // 요일
+        let day = new Date().getDay();
+        // 시간
+        let hour = new Date().getHours();
+        // 분
+        let minute = new Date().getMinutes();
+        // 오전 오후 변수
         let ampm;
 
         switch(day){
@@ -24,13 +32,15 @@ $(()=>{
             case 5 : day = '금'; break;
             case 6 : day = '토'; break;
         }
+        if(month < 10) month = '0' + month
         if(hour < 10) hour = '0' + hour
         if(hour < 13) ampm = 'AM'; else ampm = 'PM'
         if(hour > 12) hour = hour - 12
+        if(minute < 11) minute = '0' + minute
 
         $('.clock').html(`
             <h4>
-                ${year}.${hour}.${date}.${day}
+                ${year}.${month}.${date}.${day}
             </h4>
             <h4>
                 (${ampm}) ${hour} : ${minute}
